c5ad2004f5de3e4ae1c58c3843409871eeb2f8a2
BrightonYew <brightony@nycstudents.net>
2025-02-14 13:55:03 +0000

    FINAL

--- src/main/java/com/example/project/Student/Utility.java
+++ src/main/java/com/example/project/Student/Utility.java
@@ -9,29 +9,32 @@ public class Utility {
         Student earliestStudent = new Student("AAAA", "AAAAA", 1);
         int removeIndex = 0;
 
-        boolean lastEqual = false;
+        boolean lastEqual = false; //these are booleans to make sure other comparasions don't equal
         boolean firstEqual = false;
 
         while(list.size() > 0) { //repeats until there are no students left
-            earliestStudent = new Student("ZZZZZZZZZZZ", "ZZZZZZZZZZZZ", 1);
+            earliestStudent = new Student("ZZZZZZZZZZZ", "ZZZZZZZZZZZZ", 1); //sets the comparasion to a insanlly easy to override student object
             removeIndex = 0;
             for (int j = 0; j < list.size(); j++) { //goes through the entire list
 
-                lastEqual = false;
+                lastEqual = false; //resets booleans for each element
                 firstEqual = false;
 
                 if (earliestStudent.getLastName().compareTo(list.get(j).getLastName()) >= 0) { //compares the current element to the "earliest student"
-                    if (earliestStudent.getLastName().equals(list.get(j).getLastName()) && earliestStudent.getFirstName().compareTo(list.get(j).getFirstName()) >= 0) { //if it equals and the first name is (earlier / later) ????
-                        lastEqual = true;
+                    if (earliestStudent.getLastName().equals(list.get(j).getLastName())) { //if last name equals
+                        lastEqual = true; //flags true, so other comparasions don't work
                         if (earliestStudent.getGpa() == list.get(j).getGpa() && earliestStudent.getGpa() > list.get(j).getGpa()) { //if first names equal, compare gpa
-                            firstEqual = true;
-
-                            earliestStudent = list.get(j);
-                            removeIndex = j;
+                            firstEqual = true; //flags true, so other comparasions don't work
+                            if (earliestStudent.getGpa() > list.get(j).getGpa()) { //compares whether gpa is higher
+                                earliestStudent = list.get(j); //if the gpa of the comparing element is higher, set it as earlier
+                                removeIndex = j;
+                            }
                         }
-                        if (!firstEqual) { //comparasion for first name does not occur if they equal, and gpa is considered
-                            earliestStudent = list.get(j);
+                        if (!firstEqual) { //comparasion for last name does not occur if they equal, and firstname is considered
+                            if (earliestStudent.getFirstName().compareTo(list.get(j).getFirstName()) >= 0) { //if 
+                            earliestStudent = list.get(j);//if the firstname of the comparing element is earlier, set it as earlier
                             removeIndex = j; 
+                            }
                         }
                     }
                     if (!firstEqual && !lastEqual) { //comparasion for last name does not occur if they equal, and gpa / first name is considered
@@ -41,15 +44,12 @@ public class Utility {
                 }
             }
 
-            newarr.add(earliestStudent);
+            newarr.add(earliestStudent); //adds the earliest student, then removes it from the list so it is not compared to again
             list.remove(removeIndex);
             
         }
         return newarr;
     }
 
-    public static int compare(Student stu1, Student stu2) {
-        return 0;
-    }
 
 }
